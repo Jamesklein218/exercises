f7851d114ce00d660d851b56b72efb2a0b15a2fb
Tran Ngoc Dang Khoa <khoa.tranngocdang03@hcmut.edu.vn>
2021-11-15 23:21:43 +0700

    [f] - Implementing the cafe voting webapp
    - Adding Button to vote (good, neutral, bad)
    - Adding Statistic table to display vote count, ...
    - Adding styling to button

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,67 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './index.css'
 
-const Head = (props) => {
+const Head = ({ text }) => {
+  return <h1>{text}</h1>
+}
+
+const Button = ({ text, handle }) => {
   return (
-    <div class="Head">
-      <h1>{props.course}</h1>
-    </div>
+    <button onClick={handle}>{text}</button>
   )
 }
 
-const Part = ({ part, exercises }) => {
+const StatisticLine = ({ text, value }) => {
   return (
-    <p> {part} {exercises} </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
-const Content = (props) => {
-  return (
-    <div class="content">
-      <Part part={props.partList[0].name}
-        exercises={props.partList[0].exercises} />
-      <Part part={props.partList[1].name}
-        exercises={props.partList[1].exercises} />
-      <Part part={props.partList[2].name}
-        exercises={props.partList[2].exercises} />
-    </div>
-  )
+const Statistic = ({ good, bad, neutral }) => {
+  let total = good + neutral + bad
+  let average = ((good - bad) / total)
+  let positive = ((good / (total)) * 100) + " %"
+
+  if (total !== 0) {
+    return (
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} />
+        </tbody>
+      </table>
+    )
+  } else {
+    return <p>No feedback given</p>
+  }
 }
 
-const Total = (props) => {
-  return (
-    <div class="total">
-      <p>Number of exercises {props.total[0].exercises + props.total[1].exercises + props.total[2].exercises}</p>
-    </div>
-  )
-}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  console.log('render')
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
   }
 
-
   return (
-    <div class="app">
-      <Head course={course.name} />
-      <Content
-        partList={course.parts}
+    <div>
+      <Head text="give feedback" />
+      <Button text="good" handle={handleGood} />
+      <Button text="neutral" handle={handleNeutral} />
+      <Button text="bad" handle={handleBad} />
+      <Head text="statistics" />
+      <Statistic
+        good={good}
+        neutral={neutral}
+        bad={bad}
       />
-      <Total total={course.parts} />
     </div>
   )
 }
